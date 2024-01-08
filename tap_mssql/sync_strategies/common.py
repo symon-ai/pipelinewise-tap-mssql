b8a51c6bdf3d554a197e8c0eb9abe47effde565e
JiawenHou <190137434@qq.com>
2024-01-08 12:29:59 -0500

    WP-17820 set query timeout for MsSql to avoid long hanging (#12)
    * WP-17820 add connection query timeout as 60s
    
    * WP-17820 query time testing
    
    * WP-17820 extend quert_timeout to 1 hour, err handling for query timeout

--- tap_mssql/sync_strategies/common.py
+++ tap_mssql/sync_strategies/common.py
@@ -6,6 +6,9 @@ import datetime
 import singer
 import time
 import uuid
+import pymssql
+from tap_mssql.symon_exception import SymonException
+
 
 import singer.metrics as metrics
 from singer import metadata
@@ -144,60 +147,67 @@ def sync_query(cursor, catalog_entry, state, select_sql, columns, stream_version
     # query_string = cursor.mogrify(select_sql, params)
 
     time_extracted = utils.now()
-    cursor.execute(select_sql, params)
+    try:
+        cursor.execute(select_sql, params)
 
-    row = cursor.fetchone()
-    rows_saved = 0
+        row = cursor.fetchone()
+        rows_saved = 0
 
-    database_name = get_database_name(catalog_entry)
+        database_name = get_database_name(catalog_entry)
 
-    with metrics.record_counter(None) as counter:
-        counter.tags["database"] = database_name
-        counter.tags["table"] = catalog_entry.table
+        with metrics.record_counter(None) as counter:
+            counter.tags["database"] = database_name
+            counter.tags["table"] = catalog_entry.table
 
-        while row:
-            counter.increment()
-            rows_saved += 1
-            record_message = row_to_singer_record(
-                catalog_entry, stream_version, row, columns, time_extracted
-            )
-            singer.write_message(record_message)
-            md_map = metadata.to_map(catalog_entry.metadata)
-            stream_metadata = md_map.get((), {})
-            replication_method = stream_metadata.get("replication-method")
-
-            if replication_method in {"FULL_TABLE", "LOG_BASED"}:
-                key_properties = get_key_properties(catalog_entry)
-
-                max_pk_values = singer.get_bookmark(
-                    state, catalog_entry.tap_stream_id, "max_pk_values"
+            while row:
+                counter.increment()
+                rows_saved += 1
+                record_message = row_to_singer_record(
+                    catalog_entry, stream_version, row, columns, time_extracted
                 )
+                singer.write_message(record_message)
+                md_map = metadata.to_map(catalog_entry.metadata)
+                stream_metadata = md_map.get((), {})
+                replication_method = stream_metadata.get("replication-method")
 
-                if max_pk_values:
-                    last_pk_fetched = {
-                        k: v for k, v in record_message.record.items() if k in key_properties
-                    }
+                if replication_method in {"FULL_TABLE", "LOG_BASED"}:
+                    key_properties = get_key_properties(catalog_entry)
 
-                    state = singer.write_bookmark(
-                        state, catalog_entry.tap_stream_id, "last_pk_fetched", last_pk_fetched
+                    max_pk_values = singer.get_bookmark(
+                        state, catalog_entry.tap_stream_id, "max_pk_values"
                     )
 
-            elif replication_method == "INCREMENTAL":
-                if replication_key is not None:
-                    state = singer.write_bookmark(
-                        state, catalog_entry.tap_stream_id, "replication_key", replication_key
-                    )
+                    if max_pk_values:
+                        last_pk_fetched = {
+                            k: v for k, v in record_message.record.items() if k in key_properties
+                        }
 
-                    state = singer.write_bookmark(
-                        state,
-                        catalog_entry.tap_stream_id,
-                        "replication_key_value",
-                        record_message.record[replication_key],
-                    )
-            if rows_saved % 1000 == 0:
-                singer.write_message(singer.StateMessage(
-                    value=copy.deepcopy(state)))
+                        state = singer.write_bookmark(
+                            state, catalog_entry.tap_stream_id, "last_pk_fetched", last_pk_fetched
+                        )
 
-            row = cursor.fetchone()
+                elif replication_method == "INCREMENTAL":
+                    if replication_key is not None:
+                        state = singer.write_bookmark(
+                            state, catalog_entry.tap_stream_id, "replication_key", replication_key
+                        )
+
+                        state = singer.write_bookmark(
+                            state,
+                            catalog_entry.tap_stream_id,
+                            "replication_key_value",
+                            record_message.record[replication_key],
+                        )
+                if rows_saved % 1000 == 0:
+                    singer.write_message(singer.StateMessage(
+                        value=copy.deepcopy(state)))
+
+                row = cursor.fetchone()
+    except pymssql._mssql.MSSQLDatabaseException as e:
+        message = str(e)
+        
+        if "timeout" in message:
+            raise SymonException('The timeout period elapsed before completing the query execution, or the server is not responding. Please try again.', 'msSql.QueryTimeout')
+        raise
 
     singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
